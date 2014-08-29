d3d99422c69fea9ffccb5636518aa65b06609fbb
sagar <kumarsagar15795@gmail.com>
2014-08-30 00:18:05 +0530

    niesbud and msme latest done

---
+++ niesbud_training.py
@@ -0,0 +1,43 @@
+import urllib2
+from bs4 import BeautifulSoup
+import json
+
+
+class Scraper:
+    def webscraper(self):
+        url='http://niesbud.nic.in/training.htm'
+        print 'scraping %s '%(url)
+        arr =[]
+	dict = {}
+	soup = BeautifulSoup( urllib2.urlopen(url).read() )
+	i=soup.find("div",{"class":"module"} )
+	title=''
+	data=''
+	for m in i.find_all("h3"):
+		title=m.get_text()
+		data=m.find_next("p").get_text()
+		dict=return_dict(data,title)
+		arr.append(dict)
+	return arr
+       
+def return_dict(des,title):
+
+    dict = {}
+    dict['description'] = des
+    dict['title']=title
+    try:
+        print ' %s | %s '%(des,title)
+    except:
+        pass
+    return dict
+
+def main():
+    scrape = Scraper()
+    arr1 = scrape.webscraper()
+    with open("niesbud_training.txt", "w") as f:
+        f.write(json.dumps(arr1))
+
+if __name__ == '__main__':
+    main()
+    #print gen_favicon('flipkart')
+
---
+++ niesbud_workshop.py
@@ -0,0 +1,49 @@
+import urllib2
+from bs4 import BeautifulSoup
+import json
+
+class Scraper:
+    def webscraper(self):
+        url='http://niesbud.nic.in/workshop.htm'
+        print 'Now scraping %s '%(url)
+        arr =[]
+	dict = {}
+	soup = BeautifulSoup( urllib2.urlopen(url).read() )
+	i=soup.find("div",{"id":"jaipur-workshop"} )
+	#print i.find_next("div").find_next("div").get_text()
+	title=''
+        des=''
+	title= i.find_next("div").get_text()
+	#print "----------------------------------------"
+        des=i.find("p",{"align":"justify"}).get_text()
+	dict=return_dict(des,title)
+	arr.append(dict)
+	i=soup.find("div",{"id":"workshop_on_Brainstorming"} )
+	#print "-------------------------------------------"
+	title= i.find_next("h2").get_text()
+	#print "-------------------------------------------"
+	des= i.find_next("p").get_text()
+	dict=return_dict(des,title)
+	arr.append(dict)        
+	return arr
+       
+def return_dict(des,title):
+    global count
+    dict = {}
+    dict['description'] = des
+    dict['title']=title
+    try:
+        print ' %s | %s '%(des,title)
+    except:
+        pass
+    return dict
+
+def main():
+    scrape = Scraper()
+    arr1 = scrape.webscraper()
+    with open("niesbud_workshop.txt", "w") as f:
+        f.write(json.dumps(arr1))
+
+if __name__ == '__main__':
+    main()
+    
